733128a116d2983447ec74a4a6d17f038fdcf6e2
jerome roeser <jerome.roeser@gmail.com>
2024-10-29 10:19:34 +0100

    Update data structure

--- experimental_data_digitalization/params.py
+++ experimental_data_digitalization/params.py
@@ -4,7 +4,9 @@ import os
 
 repo_path = Path(__file__).parent.parent
 
-LOCAL_DATA_PATH = repo_path.joinpath('data')
+PDF_CATEGORY= os.environ.get('PDF_CATEGORY')
+LOCAL_DATA_PATH = repo_path.joinpath('data', 'pdfs', f"{PDF_CATEGORY}")
+
 
 LANGCHAIN_API_KEY= os.environ.get('LANGCHAIN_API_KEY')
 OPENAI_API_KEY= os.environ.get('OPENAI_API_KEY')
