5defc2f470a4af0b38ef0ccd17c1ac043aefeca4
jerome roeser <jerome.roeser@gmail.com>
2024-10-29 06:49:20 +0100

    Add API keys in params module

--- experimental_data_digitalization/params.py
+++ experimental_data_digitalization/params.py
@@ -1,6 +1,10 @@
 from pathlib import Path
+import os
 
 
 repo_path = Path(__file__).parent.parent
 
 LOCAL_DATA_PATH = repo_path.joinpath('data')
+
+LANGCHAIN_API_KEY= os.environ.get('LANGCHAIN_API_KEY')
+OPENAI_API_KEY= os.environ.get('OPENAI_API_KEY')
